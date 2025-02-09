7fabdec384e5b908b9b23091c5720f1c14d057c2
akashgarine <akashgarine123@gmail.com>
2025-02-09 15:05:22 +0530

    payment added

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config"
 import projectRouter from "./routes/project.route.js";
 import userRouter from "./routes/user.route.js";
+import paymentRouter from "./routes/payment.route.js";
 import resourceRouter from "./routes/resource.route.js"
 import connectDb from "./utils/connectDb.js";
 import issueRouter from "./routes/issue.route.js";
@@ -30,8 +31,7 @@ app.use("/api/auth",userRouter);
 app.use("/api/project",projectRouter);
 app.use("/api/resources",resourceRouter);
 app.use("/api/issues",issueRouter);
-
-
+app.use("/api/payments",paymentRouter);
 //listening on port
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port `)
